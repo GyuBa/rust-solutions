3690d833e50d6ce3f2d794a709e35a316aee57f2
GyuBa <sansam1010@naver.com>
2024-08-09 00:30:35 +0900

    echo command

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,34 +1,24 @@
-use clap::{Arg, ArgAction, Command};
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+// Rust version of 'echo'
+struct Args {
+    #[arg(required(true))]
+    text: Vec<String>,
+
+    #[arg(short('n'))]
+    omit_newline: bool,
+}
 
 fn main() {
-    let matches = Command::new("echor")
-    .version("0.1.0")
-    .author("GYU")
-    .about("Rust version of 'echo'")
-    .arg(
-        Arg::new("text")
-        .value_name("TEXT")
-        .help("Input test")
-        .required(true)
-        .num_args(1..),
-    )
-    .arg(
-        Arg::new("omit_newline")
-        .short('n')
-        .action(ArgAction::SetTrue)
-        .help("Do not print newline")
-    )
-    .get_matches();
-
-    let text: Vec<String> = matches.get_many("text").unwrap().cloned().collect();
-    let omit_newline = matches.get_flag("omit_newline");
-
-    if omit_newline == true {
-        //        println!("[TRUE] : OMIT_NEWLINE");
-        print!("{}\n", text.join(" "));
-    } else {
-//        println!("[FALSE] : OMIT_NEWLINE");
-        print!("{}", text.join(" "));
-    }
+    let args = Args::parse();
+    //    dbg!(args);
+    print!(
+        "{}{}",
+        args.text.join(" "),
+        if args.omit_newline { "" } else { "\n" }
+        );
 
 }
+
